992e5fa78b3a0b74c6e3fe40555d9900bacbf9ba
DESKTOP-9H7KJTE\hvenu <hvenura@gmail.com>
2026-05-28 10:49:40 +1000

    initial commit of WIlderness application prototype

---
+++ parks.js
@@ -0,0 +1,99 @@
+const parkImg = document.getElementById("parkImg");
+const parkName = document.getElementById("parkName");
+const parkDesc = document.getElementById("parkDescription");
+const parkAddress = document.getElementById("parkAddress");
+const btn = document.getElementById("pagebutton");
+
+// btn.addEventListener("click", (event) => {
+//   const parkCode = event.target.value;
+//   loadNewPark(parkCode);
+// });
+
+async function loadNewPark(parkCode) {
+  try {
+    // https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=GCXOTF0JIuC5ZmdRXUcceCboJXXlKFPN9Yd14DOD
+    //
+    const data = await fetch(
+      `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=GCXOTF0JIuC5ZmdRXUcceCboJXXlKFPN9Yd14DOD`,
+    );
+    const json = await data.json();
+    console.log(json);
+    displayData(json.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log("About to Fetch some data");
+async function load() {
+  try {
+    // https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=GCXOTF0JIuC5ZmdRXUcceCboJXXlKFPN9Yd14DOD
+    // `https://developer.nps.gov/api/v1/parks?parkCode=${park}&api_key=GCXOTF0JIuC5ZmdRXUcceCboJXXlKFPN9Yd14DOD`
+    const data = await fetch(
+      "https://developer.nps.gov/api/v1/parks?limit=5&api_key=GCXOTF0JIuC5ZmdRXUcceCboJXXlKFPN9Yd14DOD",
+    );
+    const json = await data.json();
+    console.log(json);
+    displayData(json.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayData(data) {
+  parkImg.setAttribute("src", data[0].images[0].url);
+  parkImg.setAttribute("alt", data[0].images[0].altText);
+
+  parkName.innerHTML = data[0].name;
+  parkDesc.innerHTML = data[0].description;
+  parkAddress.innerHTML =
+    data[0].addresses[0].line1 +
+    " " +
+    data[0].addresses[0].city +
+    " " +
+    data[0].addresses[0].countryCode;
+
+  // for (let index = 0; index < data.length; index++) {
+  //   console.log(data[index].description);
+  //   console.log(data[index].name);
+
+  //   console.log(data[index].images[0].url);
+  // }
+}
+
+// function displayPark() {
+
+//   try {
+//     const data = await fetch( `https://developer.nps.gov/api/v1/parks?parkCode=${park}&api_key=GCXOTF0JIuC5ZmdRXUcceCboJXXlKFPN9Yd14DOD`);
+
+//     const json = await data.json();
+//   }
+//   catch (error) {
+
+//  console.log(error);
+
+//   }
+
+// }
+// when a pagination button is clicked -> search for park
+// create onClick event for all pagination buttons
+// the evert then passes the pagination button value, ie the park, to the fetch function
+// when we find the newPark, replace data with newPark.data
+// we do this by assigning innerHTML and attributes to the ids
+
+async function getEvents(parkcode) {
+  try {
+    const data = await fetch(
+      `https://developer.nps.gov/api/v1/events?parkCode=${parkcode}&api_key=GCXOTF0JIuC5ZmdRXUcceCboJXXlKFPN9Yd14DOD`,
+    );
+    const json = await data.json();
+    console.log(json);
+    displayEvents(json.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayEvents(data) {}
+
+load();
